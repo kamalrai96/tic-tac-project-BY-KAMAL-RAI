eef6f1f44dd8b63dc12f2401a396dfa87b9a949b
kamalrai96 <108433901+kamalrai96@users.noreply.github.com>
2022-11-08 15:51:29 +0530

    Add files via upload

---
+++ script-k.js
@@ -0,0 +1,30 @@
+// console.log("Welcome To Tic Tac Toe")
+// let music = new Audio("musix.mp3")
+// let audioTurn = new Audio("ting.mp3")
+// let gameover = new Audio("gameover.mp3")
+// let turn = "X";
+
+// // function 2 change the turn
+// const changeTurn = () => {
+//     return trun === "X" ? "0" : "X"
+// }
+
+// // function check win
+// const checkWin = () => {
+
+// }
+
+// // game Code
+// let boxes = document.getElementsByClassName("box");
+// Array.from(boxes).forEach(Element => {
+//     let boxtext = element.querySelector('.boxtext');
+//     element.addEventListener('click', () =>{
+//         if (boxtext.innerText === '') {
+//             boxtext.innerText = turn;
+//             changeTurn();
+//             audioTurn.play();
+//             checkWin();
+//             document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+//         }
+//     })
+// })
